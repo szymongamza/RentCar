cdb21c1854ee1fe9ea688dc327b693d68c46f3cc
Szymon Gamża <gamza.szymon@gmail.com>
2023-04-21 01:31:01 +0200

    Added validation

--- RentCar.API/Resources/Manufacturer/SaveManufacturerResource.cs
+++ RentCar.API/Resources/Manufacturer/SaveManufacturerResource.cs
@@ -1,6 +1,10 @@
-﻿namespace RentCar.API.Resources.Manufacturer;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentCar.API.Resources.Manufacturer;
 
 public class SaveManufacturerResource
 {
+    [Required]
+    [MaxLength(30)]
     public string ManufacturerName { get; set; }
 }
