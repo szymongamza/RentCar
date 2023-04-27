9307142d4cbbeb03213834acb1763d394e93c112
Szymon Gamża <gamza.szymon@gmail.com>
2023-04-28 01:29:30 +0200

    Changed name.

--- RentCar.UI/src/components/NavBar.tsx
+++ RentCar.UI/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function BasicExample() {
+function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -19,4 +19,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default NavBar;
