2ae06dfda9be50b762297f86e817826f5bc9e4de
Szymon Gamża <gamza.szymon@gmail.com>
2023-05-02 03:42:11 +0200

    Swagger in production

--- RentCar.API/Program.cs
+++ RentCar.API/Program.cs
@@ -23,11 +23,10 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors(options =>
     options.WithOrigins("http://localhost:3000")
